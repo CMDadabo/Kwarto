cf9ad2489e920101481e0ab91895eb7febcc2d6c
Christopher M. Dadabo <cmdadabo@gmail.com>
2013-08-08 23:00:18 -0500

    Add Board class, change Piece class, wrap code in functions
    Create proper main loop
    Add Board class with core game functions
    Adjust small game piece size

--- bin/app/game.js
+++ bin/app/game.js
@@ -14,12 +14,65 @@ var create_board = function () {
 	$(".board_space").height(space_size);
 };
 
+var Board = function () {
+	// Board-related variables
+	var lines = new Object();
+	lines.col1 = [0,4,8,12];
+	lines.col2 = [1,5,9,13];
+	lines.col3 = [2,6,10,14];
+	lines.col4 = [3,7,11,15];
+
+	lines.row1 = [0,1,2,3];
+	lines.row2 = [4,5,6,7];
+	lines.row3 = [8,9,10,11];
+	lines.row4 = [12,13,14,15];
+
+	lines.diag1 = [0,5,10,15];
+	lines.diag2 = [3,6,9,12];
+
+	this.check_line = function (line) {
+
+		var pieces_to_check = new Array();
+
+		for (var i = 0; i < 4 ; i++) {
+			var p = board["space_" + line[i]];
+			if (board.hasOwnProperty('space_' + line[i])) {
+				pieces_to_check.push(p);
+				// console.log("Pushing " + p + " from [space_" + line[i] + "]");
+			};
+		}
+		// console.log(pieces_to_check);
+		// console.log(pieces_to_check[0].attrs);
+		if (pieces_to_check.length == 4) {
+			for (var i = 0; i < 4 ; i++){
+				if (pieces_to_check[0].attrs[i] == pieces_to_check[1].attrs[i] &&
+					pieces_to_check[1].attrs[i] == pieces_to_check[2].attrs[i] &&
+					pieces_to_check[2].attrs[i] == pieces_to_check[3].attrs[i]) {
+				return true;
+				}
+			}
+		}
+		return false;
+	};
+
+	this.check_win = function () {
+		for (line_name in lines) {
+			// console.log(line_name);
+			if (this.check_line(lines[line_name])) { return true };
+		}
+		return false;
+	};
+}
+
+
 var Piece = function (id, big, dark, square, solid) {
 	'use strict';
 	this.big = big;
 	this.dark = dark;
 	this.square = square;
 	this.solid = solid;
+	this.ID = id.toString();
+	this.attrs = [this.big, this.dark, this.square, this.solid];
 	this.$pieceDiv = $('<div class="piece" id="' + id + '"></div>');
 
 	// Size
@@ -56,7 +109,15 @@ var choose_piece = function ($piece) {
 	$piece.addClass('chosen');
 };
 
+// var place_piece = function (targetSquare, pieceID) {
+// 	'use strict';
+// 	$( this ).append( $('#' + pieceID) );
+// 	$('.chosen').removeClass('chosen');
+// 	ai_turn();
+// };
+
 var pieces = [];
+var remaining_pieces = [];
 
 var fill_sideboard = function () {
 	'use strict';
@@ -76,17 +137,53 @@ var fill_sideboard = function () {
 	pieces.push(new Piece(13, false, true, false, false));
 	pieces.push(new Piece(14, true, false, false, false));
 	pieces.push(new Piece(15, false, false, false, false));
-	console.log("Sideboard full")
+	console.log("Sideboard full");
+	remaining_pieces = pieces;
 };
-
 
 var draw_sideboard = function () {
 	'use strict';
 	for (var i = 0; i<pieces.length; i++) {
 		$('.sideboard').append(pieces[i].$pieceDiv);
-		console.log("Drawing piece id=" + i);
 	}
 };
+
+var ai_pick_piece = function () {
+	console.log("Game won: " + board.check_win());
+	var random_choice = Math.floor(Math.random()*remaining_pieces.length);
+	choose_piece( $( '#' +  remaining_pieces[random_choice].ID) );
+	console.log("Piece " + random_choice + " chosen.");
+	console.log("Choose where to place this piece.");
+}
+
+var player_place_piece = function () {
+	$('.board_space').click( function () {
+		if (!$( this ).children().hasClass('piece')) {
+			$( this ).append( $('.chosen') );
+			var target_square = $( this ).attr('id');
+			console.log(target_square);
+
+			var placed_id = $('.chosen').attr('id');
+			console.log("Piece " + placed_id + " placed.");
+			$('.chosen').removeClass('chosen');
+
+			for (var i = 0; i<remaining_pieces.length; i++){
+				if (remaining_pieces[i].ID === placed_id) {
+					board[target_square] = remaining_pieces.splice(i, 1)[0];
+					console.log("Piece " + placed_id + " removed from remaining_pieces array.");
+				}
+			};
+			ai_pick_piece();
+		} else { console.log ( "Space occupied." ) };
+
+	});
+};
+
+var player_pick_piece = function () {
+	$
+}
+
+var board = new Board();
 
 // Initialize game
 $(document).ready(function () {
@@ -95,17 +192,6 @@ $(document).ready(function () {
 	draw_sideboard();
 
 	// Main loop
-	while (!game_over){
-		if (current_turn == TURN_AI) {
-			var random_choice = Math.floor(Math.random()*16);
-			choose_piece( $( '#' + random_choice ) );
-			current_turn == TURN_PLAYER; }
-		// } else if (current_turn == TURN_PLAYER) {
-		// 	$('.board_space').click( function () {
-		// 		$'.chosen'.hide
-		// 	})
-		// 	game_over = true;
-		// };
-	};
-
+	ai_pick_piece();
+	player_place_piece();
 });
